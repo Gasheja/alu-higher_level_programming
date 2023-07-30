18eff020d5869225e5f7d6138c839794c0ed4c72
Gasheja <m.gasheja@alustudent.com>
2023-07-30 05:25:42 -0700

    my privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on my server
-SHOW GRANTS FOR user_0d_1inlocalhost;
-SHOW GRANTS FOR user_0d_2inlocalhost;
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
