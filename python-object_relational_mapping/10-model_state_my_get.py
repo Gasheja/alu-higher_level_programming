0e75f57ffbeb6d2f4ebefc5447a142bbc7a1d7c0
Gasheja <m.gasheja@alustudent.com>
2023-08-16 12:06:03 -0700

    state get

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""
-Lists all State objects that contain the letter a
-"""
+"""Lists the State object with the name passed as argument."""
+
 import sys
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from model_state import Base, State
 
-
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(username, password, db_name),
-                           pool_pre_ping=True)
+    # setup connection to db
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(mysql_username, mysql_password, database_name),
+        pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    result = session.query(State).filter(State.name.like('%a%')).all()
+    # query for state object with name
+    state = session.query(State).filter(State.name == state_name).first()
 
-    for state in result:
-        print("{}: {}".format(state.id, state.name))
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+    session.close()
