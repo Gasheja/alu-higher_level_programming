4e6a469d5e75c14a4e23101e9bdcb67716b1d899
Gasheja <m.gasheja@alustudent.com>
2023-10-03 05:34:50 -0700

    star wars characters

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -2,49 +2,16 @@
 
 const request = require('request');
 
-if (process.argv.length > 2) {
-  const movieId = process.argv[2];
-  const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-  request.get(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error(`An error occurred while making the request: ${error}`);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-      return;
-    }
-
-    try {
-      const movie = JSON.parse(body);
-      const charactersUrls = movie.characters;
-
-      const printCharacters = (urls, index = 0) => {
-        if (index >= urls.length) {
-          return;
-        }
-
-        request.get(urls[index], (error, response, body) => {
-          if (error) {
-            console.error(`An error occurred while making the request: ${error}`);
-          } else if (response.statusCode !== 200) {
-            console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-          } else {
-            const character = JSON.parse(body);
-            console.log(character.name);
-          }
-
-          printCharacters(urls, index + 1);
-        });
-      };
-
-      printCharacters(charactersUrls);
-    } catch (error) {
-      console.error(`An error occurred while parsing the response: ${error}`);
-    }
+request('http://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  JSON.parse(body).characters.forEach(function (url, callback) {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.error(error);
+      }
+      console.log(JSON.parse(body).name);
+    });
   });
-} else {
-  console.log('Please provide the Movie ID as an argument.');
-}
+});
